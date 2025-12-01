5f25201d6afbe4bdb3fbf2d7474fc62ed0747a70
Shane Mc Cormack <shanemcc@gmail.com>
2025-12-01 05:18:21 +0000

    Day 1. Part 2.

--- 1/run.php
+++ 1/run.php
@@ -11,22 +11,31 @@
 	}
 
 	$startValue = 50;
-	$dialSize = 99;
+	$dialSize = 100; // 0 - 99
 
-	$part1 = 0;
+	$part1 = $part2 = 0;
 	$value = $startValue;
 
 	foreach ($rotations as $r) {
+		$repeats = floor($r[1] / $dialSize);
+		$r[1] -= $repeats * $dialSize;
+		$part2 += $repeats;
+		$oldValue = $value;
+
 		if ($r[0] == 'R') {
 			$value += $r[1];
 		} else {
 			$value -= $r[1];
 		}
-		$value = wrapmod($value, $dialSize + 1);
+
+		if ($oldValue != 0 && ($value < 0 || $value >= $dialSize || $value == 0)) {
+			$part2++;
+		}
+
+		$value = wrapmod($value, $dialSize );
+
 		if ($value == 0) { $part1++; }
 	}
 
 	echo 'Part 1: ', $part1, "\n";
-
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $part2, "\n";
