7d63678fd97393486fea6b14830b2728e46c0f3c
Shane Mc Cormack <shanemcc@gmail.com>
2025-12-03 11:11:31 +0000

    Day 3. Part 2.

--- 3/run.php
+++ 3/run.php
@@ -3,7 +3,7 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	function getJoltage($line) {
+	function getBasicJoltage($line) {
 		$len = strlen($line);
 		$first = -1;
 		$second = -1;
@@ -24,12 +24,37 @@
 		return ($first * 10) + $second;
 	}
 
+	function getExtraJoltage($line, $amount = 12) {
+		$len = strlen($line);
+
+		$values = array_fill(1, $amount, -1);
+
+		for ($i = 0; $i < $len; $i++) {
+			$val = $line[$i];
+			for ($j = 1; $j <= $amount; $j++) {
+				if ($i < $len - ($amount - $j)) {
+					if ($val > $values[$j]) {
+						$values[$j] = $val;
+
+						for ($k = $j + 1; $k <= $amount; $k++) {
+							$values[$k] = -1;
+						}
+
+						continue 2;
+					}
+				}
+			}
+		}
+
+		return (int)implode("", $values);
+	}
+
 	$part1 = 0;
+	$part2 = 0;
 	foreach ($input as $line) {
-		$part1 += getJoltage($line);
+		$part1 += getBasicJoltage($line);
+		$part2 += getExtraJoltage($line);
 	}
 
 	echo 'Part 1: ', $part1, "\n";
-
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $part2, "\n";
