cd32b3e99b0b44426fd834a91ad123b5203d9509
Shane Mc Cormack <shanemcc@gmail.com>
2025-12-03 11:12:10 +0000

    Dedupe.

--- 3/run.php
+++ 3/run.php
@@ -3,40 +3,19 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	function getBasicJoltage($line) {
-		$len = strlen($line);
-		$first = -1;
-		$second = -1;
-		for ($i = 0; $i < $len; $i++) {
-			if ($i < ($len - 1)) {
-				if ($line[$i] > $first) {
-					$first = $line[$i];
-					$second = $line[$i + 1];
-					continue;
-				}
-			}
-
-			if ($line[$i] > $second) {
-				$second = $line[$i];
-			}
-		}
-
-		return ($first * 10) + $second;
-	}
-
-	function getExtraJoltage($line, $amount = 12) {
+	function getJoltage($line, $batteries) {
 		$len = strlen($line);
 
-		$values = array_fill(1, $amount, -1);
+		$values = array_fill(1, $batteries, -1);
 
 		for ($i = 0; $i < $len; $i++) {
 			$val = $line[$i];
-			for ($j = 1; $j <= $amount; $j++) {
-				if ($i < $len - ($amount - $j)) {
+			for ($j = 1; $j <= $batteries; $j++) {
+				if ($i < $len - ($batteries - $j)) {
 					if ($val > $values[$j]) {
 						$values[$j] = $val;
 
-						for ($k = $j + 1; $k <= $amount; $k++) {
+						for ($k = $j + 1; $k <= $batteries; $k++) {
 							$values[$k] = -1;
 						}
 
@@ -52,8 +31,8 @@
 	$part1 = 0;
 	$part2 = 0;
 	foreach ($input as $line) {
-		$part1 += getBasicJoltage($line);
-		$part2 += getExtraJoltage($line);
+		$part1 += getJoltage($line, 2);
+		$part2 += getJoltage($line, 12);
 	}
 
 	echo 'Part 1: ', $part1, "\n";
