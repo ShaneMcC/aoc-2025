4cbff93a330d0d6ba1ae7e59b3469b5fa7ff1f17
Shane Mc Cormack <shanemcc@gmail.com>
2025-12-04 05:13:33 +0000

    Day 4. Part 2.

--- 4/run.php
+++ 4/run.php
@@ -5,18 +5,34 @@
 
 	$part1 = 0;
 
-	foreach (cells($map) as [$x, $y, $cell]) {
-		$counts = [];
-		foreach (getAdjacentCells($map, $x,$y, true) as [$aX, $aY]) {
-			$aC = $map[$aY][$aX] ?? '.';
-			$counts[$aC] = ($counts[$aC] ?? 0) + 1;
+	function removeRolls($map) {
+		$count = 0;
+		$newMap = $map;
+
+		foreach (cells($map) as [$x, $y, $cell]) {
+			$counts = [];
+			foreach (getAdjacentCells($map, $x,$y, true) as [$aX, $aY]) {
+				$aC = $map[$aY][$aX] ?? '.';
+				$counts[$aC] = ($counts[$aC] ?? 0) + 1;
+			}
+
+			if ($cell == '@' && ($counts['@'] ?? 0) < 4) {
+				$count++;
+				$newMap[$y][$x] = '.';
+			}
 		}
 
-		if ($cell == '@' && ($counts['@'] ?? 0) < 4) {
-			$part1++;
-		}
+		return [$count, $newMap];
 	}
+
+	$part1 = removeRolls($map)[0];
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	$part2 = 0;
+	$count = 0;
+
+	do {
+		[$count, $map] = removeRolls($map);
+		$part2 += $count;
+	} while ($count != 0);
+	echo 'Part 2: ', $part2, "\n";
