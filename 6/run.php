2ddc8d09d67c9ae4078e1e0fbd07a694288a6296
Shane Mc Cormack <shanemcc@gmail.com>
2025-12-06 20:21:41 +0000

    Day 6. Part 2.

--- 6/run.php
+++ 6/run.php
@@ -1,31 +1,57 @@
 #!/usr/bin/php
 <?php
 	require_once(dirname(__FILE__) . '/../common/common.php');
-	$input = getInputLines();
+
+	// Obviously this is a map... totally.
+	$input = getInputMap();
+
+	function calculateTotal($problems) {
+		$result = 0;
+
+		foreach ($problems as $problem) {
+			if ($problem['operation'] == '+') {
+				$result += array_sum($problem['values']);
+			} else if ($problem['operation'] == '*') {
+				$result += array_product($problem['values']);
+			}
+		}
+
+		return $result;
+	}
 
 	$part1 = 0;
 	$problems = [];
 	foreach ($input as $line) {
-		$bits = preg_split('/\s+/', trim($line));
+		$bits = preg_split('/\s+/', trim(implode('', $line)));
 		for ($i = 0; $i < count($bits); $i++) {
 			if (is_numeric($bits[$i])) {
 				$problems[$i]['values'][] = $bits[$i];
 			} else {
 				$problems[$i]['operation'] = $bits[$i];
-
-				if ($bits[$i] == '+') {
-					$problems[$i]['total'] = array_sum($problems[$i]['values']);
-				} else if ($bits[$i] == '*') {
-					$problems[$i]['total'] = array_product($problems[$i]['values']);
-				}
-
-				$part1 += $problems[$i]['total'];
 			}
 		}
 	}
+
+	$part1 = calculateTotal($problems);
 	echo 'Part 1: ', $part1, "\n";
 
+	$problems = [];
+	$problemId = -1;
+	$lastRow = array_pop($input);
+	[$minX, $minY, $maxX, $maxY] = getBoundingBox($input);
+	for ($i = 0; $i <= $maxX; $i++) {
+		$last = $lastRow[$i] ?? '';
+		if ($last == '+' || $last == '*') {
+			$problemId++;
+			$problems[$problemId]['operation'] = $lastRow[$i];
+		}
 
+		$col = array_column($input, $i);
+		$val = trim(implode('', $col));
+		if ($val !== '') {
+			$problems[$problemId]['values'][] = $val;
+		}
+	}
 
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	$part2 = calculateTotal($problems);
+	echo 'Part 2: ', $part2, "\n";
