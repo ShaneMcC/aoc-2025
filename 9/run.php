1f116e913b75a5a031bb106e0b2d17b2876686bf
Shane Mc Cormack <shanemcc@gmail.com>
2025-12-09 14:25:54 +0000

    Day 9. Part 2.

--- 9/run.php
+++ 9/run.php
@@ -4,28 +4,66 @@
 	$input = getInputLines();
 
 	$tiles = [];
+	$lines = [];
+	$prev = explode(',', $input[count($input) - 1], 2);
 	foreach ($input as $line) {
-		preg_match('#(.*),(.*)#ADi', $line, $m);
-		[$all, $x, $y] = $m;
-		$tiles[] = [$x, $y];
+		$tiles[] = [$x, $y] = explode(',', $line, 2);
+		[$pX, $pY] = $prev;
+
+		if ($pX == $x) {
+			$lines[] = ['direction' => 'vertical', 'x' => $x, 'y1' => min($y, $pY), 'y2' => max($y, $pY)];
+		} else if ($pY == $y) {
+			$lines[] = ['direction' => 'horizontal', 'y' => $y, 'x1' => min($x, $pX), 'x2' => max($x, $pX)];
+		} else {
+			die('Bad Input' . "\n");
+		}
+
+		$prev = [$x, $y];
 	}
 
 	function area($x1, $y1, $x2, $y2) {
 		return (abs($x1 - $x2) + 1) * (abs($y1 - $y2) + 1);
 	}
 
+	function isValidArea($x1, $y1, $x2, $y2) {
+		global $lines;
+
+		$minX = min($x1, $x2);
+		$maxX = max($x1, $x2);
+		$minY = min($y1, $y2);
+		$maxY = max($y1, $y2);
+
+		foreach ($lines as $line) {
+			if ($line['direction'] == 'vertical') {
+				if ($line['x'] > $minX && $line['x'] < $maxX && $line['y1'] < $maxY && $line['y2'] > $minY) {
+					return false;
+				}
+			} else if ($line['direction'] == 'horizontal') {
+				if ($line['y'] > $minY && $line['y'] < $maxY && $line['x1'] < $maxX && $line['x2'] > $minX) {
+					return false;
+				}
+			}
+		}
+
+        return true;
+	}
+
 	$part1 = PHP_INT_MIN;
+	$part2 = PHP_INT_MIN;
 
 	for ($i = 0; $i < count($tiles); $i++) {
 		for ($j = $i + 1; $j < count($tiles); $j++) {
 			[$iX, $iY] = $tiles[$i];
 			[$jX, $jY] = $tiles[$j];
 
-			$part1 = max($part1, area($iX, $iY,$jX, $jY));
+			$area = area($iX, $iY,$jX, $jY);
+			$part1 = max($part1, $area);
+
+			if ($area > $part2 && isValidArea($iX, $iY,$jX, $jY)) {
+				$part2 = max($part2, $area);
+			}
 		}
 	}
 
 	echo 'Part 1: ', $part1, "\n";
-
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $part2, "\n";
