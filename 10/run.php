cc154af3a2220d2b23976ca64dc7278408487f8f
Shane Mc Cormack <shanemcc@gmail.com>
2025-12-10 12:13:21 +0000

    Speed up part 1.

--- 10/run.php
+++ 10/run.php
@@ -8,44 +8,42 @@
 		preg_match('/\[([.#]+)\] (.*) \{(.*)\}/ADi', $line, $m);
 		[$all, $lights, $allButtons, $joltages] = $m;
 
+		// Convert lights to a binary value for the target
+		$lights = bindec(strrev(implode('', array_map(fn($a) => ($a == '#' ? 1 : 0), str_split($lights)))));
+
+		// Convert buttons to a number representing which bits they change.
 		$buttons = [];
 		$allButtons = explode(' ', $allButtons);
 		foreach ($allButtons as $b) {
-			$buttons[] = explode(',', trim($b, '()'));
+			$value = 0;
+			foreach (explode(',', trim($b, '()')) as $b) {
+				$value = $value | (1 << $b);
+			}
+			$buttons[] = $value;
 		}
 
-		$entries[] = [str_split($lights), $buttons, explode(',', $joltages)];
+		$entries[] = [$lights, $buttons, explode(',', $joltages)];
 	}
 
 	function getButtonPresses($machine) {
-		[$targetLights, $buttons, $joltages] = $machine;
+		[$targetLights, $buttons, $targetJoltages] = $machine;
 
-		$startLights = array_fill(0, count($targetLights), '.');
+		$startLights = 0;
 
 		$queue = new SplPriorityQueue();
 		$queue->setExtractFlags(SplPriorityQueue::EXTR_DATA);
-
-		$queue->insert(['lights' => $startLights, 'presses' => []], 0);
+		$queue->insert(['lights' => $startLights, 'presses' => 0], 0);
 
 		while (!$queue->isEmpty()) {
 			['lights' => $lights, 'presses' => $presses] = $queue->extract();
 
-			foreach ($buttons as $buttonId => $button) {
-				$newLights = $lights;
-
-				foreach ($button as $light) {
-					if ($newLights[$light] == '#') {
-						$newLights[$light] = '.';
-					} else {
-						$newLights[$light] = '#';
-					}
-				}
-
-				$newPresses = array_merge($presses, [$buttonId]);
-				$queue->insert(['lights' => $newLights, 'presses' => $newPresses], 0 - count($newPresses));
+			$newPresses = $presses + 1;
+			foreach ($buttons as $button) {
+				$newLights = $lights ^ $button;
+				$queue->insert(['lights' => $newLights, 'presses' => $newPresses], 0 - $newPresses);
 
 				if ($newLights == $targetLights) {
-					return count($newPresses);
+					return $newPresses;
 				}
 			}
 		}
@@ -53,14 +51,12 @@
 		return 0;
 	}
 
-	$part1 = 0;
+	$part1 = $part2 = 0;
 	$i = 0;
 	foreach ($entries as $machine) {
-		// echo $i++, ' / ', count($entries), "\n";
+		if (isDebug()) { echo $i++, ' / ', count($entries), "\n"; }
 		$part1 += getButtonPresses($machine);
 	}
 
 	echo 'Part 1: ', $part1, "\n";
-
-	// $part2 = 0;
 	// echo 'Part 2: ', $part2, "\n";
