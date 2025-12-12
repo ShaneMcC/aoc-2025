0816c9f94d6bc1f5e2dce2995cb8c2f615f62399
Shane Mc Cormack <shanemcc@gmail.com>
2025-12-12 11:41:19 +0000

    Super simplify.

--- 12/run.php
+++ 12/run.php
@@ -4,36 +4,15 @@
 	$input = getInputLineGroups();
 	$sizes = array_pop($input);
 
-	/* $shapes = [];
-	foreach ($input as $shape) {
-		array_shift($shape);
-		// $s = ['shape' => array_map(fn($x) => str_split($x), $shape), 'count' => 0];
-		// $s['count'] = count(findCells($s['shape'], '#'));
-		$shapes[] = ['count' => count_chars(implode('', $shape))[ord('#')]];
-	} */
-
-	$trees = [];
-	foreach ($sizes as $size) {
-		preg_match('#(.*): (.*)#ADi', $size, $m);
-		[$all, $size, $wanted] = $m;
-		[$w, $h] = explode('x', $size);
-		$trees[] = ['area' => (int)$w*$h, 'w' => (int)$w, 'h' => (int)$h, 'wanted' => array_map(fn($x) => (int)$x, explode(' ', $wanted))];
-	}
-
 	$part1 = 0;
 
-	foreach ($trees as $tree) {
-		/* $needed = 0;
-		foreach ($tree['wanted'] as $i => $count) {
-			$needed += $shapes[$i]['count'] * $count;
-			if ($needed > $tree['area']) { continue 2; }
-		} */
+	foreach ($sizes as $size) {
+		preg_match('#(.*)x(.*): (.*)#ADi', $size, $m);
+		[$all, $w, $h, $wanted] = $m;
+		$area = ($w * $h);
 
-		$needed = array_sum($tree['wanted']) * 9;
-		if ($needed >= $tree['area']) { continue; }
-
-		// For the actual input, apparently, this is enough?
-		// Doesn't work for the test input though...
+		$needed = array_sum(explode(' ', $wanted)) * 9;
+		if ($needed > $area) { continue; }
 		$part1++;
 	}
 
